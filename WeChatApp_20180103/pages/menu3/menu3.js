8775a0be6b0ae045585c6f065d2f546f39b64836
H-OO <hjyuanyy@gmail.com>
2018-05-24 21:00:27 +0800

    add content

--- WeChatApp_20180103/pages/menu3/menu3.js
+++ WeChatApp_20180103/pages/menu3/menu3.js
@@ -1,1 +1,24 @@
-Page({})
+Page({
+  data: {
+    value: '00 11 2222'
+  },
+  inputHandler: function (e) {
+    console.log(e);
+    // 获取value值
+    let value = e.detail.value;
+
+    // 同步data value
+    this.setData({
+      value
+    });
+  },
+  focusHandler: function (e) {
+    console.log(e);
+    // 获取value值
+    let value = e.detail.value;
+    console.log(value);
+    this.setData({
+      value
+    });
+  }
+})
